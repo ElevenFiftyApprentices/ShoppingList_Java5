47f4a64a3d3971713c2a00a3174f45e6e75c1bf2
summerpratt <summeropratt@gmail.com>
2016-11-29 16:01:20 -0500

    running app

--- src/main/java/org/elevenfifty/shoppingapp/repository/ListsRepository.java
+++ src/main/java/org/elevenfifty/shoppingapp/repository/ListsRepository.java
@@ -2,7 +2,9 @@ package org.elevenfifty.shoppingapp.repository;
 
 import org.elevenfifty.shoppingapp.beans.Lists;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ListsRepository extends CrudRepository<Lists, Long> {
 
 }
--- src/main/java/org/elevenfifty/shoppingapp/repository/NotesRepository.java
+++ src/main/java/org/elevenfifty/shoppingapp/repository/NotesRepository.java
@@ -2,7 +2,9 @@ package org.elevenfifty.shoppingapp.repository;
 
 import org.elevenfifty.shoppingapp.beans.Notes;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotesRepository extends CrudRepository<Notes, Long> {
 
 }
