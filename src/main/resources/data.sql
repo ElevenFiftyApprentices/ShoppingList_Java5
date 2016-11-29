ed74756fd08208b0294d31284256cf577236a85d
summerpratt <summeropratt@gmail.com>
2016-11-29 16:45:40 -0500

    added changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ insert into ShoppingList.lists (user_id, name, color, created_utc, modified_utc)
 
 insert into ShoppingList.items (shopping_list_id, contents, priority, is_checked, created_utc, modified_utc) values (1, 'A lovely item', 'It can wait', TRUE, '2016-11-29', '2016-12-1');
 
-insert into ShoppingList.notes (shopping_list_item_id, body, created_utc, modified_utc) values (1, 'Hey, this is the note body.', '2016-11-29', '2016-12-1' );
+insert into ShoppingList.notes (shopping_list_item_id, body, created_utc, modified_utc) values (1, 'Hey, this is the note body.', '2016-11-29', '2016-12-1');
