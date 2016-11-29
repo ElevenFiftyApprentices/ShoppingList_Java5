91dba096060a648b08bab8263d0102d604558ebe
summerpratt <summeropratt@gmail.com>
2016-11-29 15:15:16 -0500

    added html outlines

---
+++ src/main/java/org/elevenfifty/shoppingapp/controller/IndexController.java
@@ -0,0 +1,10 @@
+package org.elevenfifty.shoppingapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+}
