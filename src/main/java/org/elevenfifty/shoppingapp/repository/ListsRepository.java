46a489089579735ecf51dea6a01fa7bb6f85bb9b
John <317horus@gmail.com>
2016-11-29 15:14:58 -0500

    added repositories

---
+++ src/main/java/org/elevenfifty/shoppingapp/repository/ListsRepository.java
@@ -0,0 +1,8 @@
+package org.elevenfifty.shoppingapp.repository;
+
+import org.elevenfifty.shoppingapp.beans.Lists;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ListsRepository extends CrudRepository<Lists, Long> {
+
+}
